b6fc136caab519217a5a713d3b67107d91a365cb
unknown <angelcotes@gmail.com>
2016-04-18 15:58:43 -0500

    final proyect

---
+++ RecycleView/app/src/main/java/angelcotes/recycleview/Information.java
@@ -0,0 +1,12 @@
+package angelcotes.recycleview;
+
+/**
+ * Created by Angel on 13/04/2016.
+ */
+public class Information {
+    String title;
+
+    public Information(String title) {
+        this.title = title;
+    }
+}
